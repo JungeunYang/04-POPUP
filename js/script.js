7d378e3512253e00c71c4878aeb525d7a3b6ca30
jungeun <yangje1113@naver.com>
2023-12-04 12:57:09 +0900

    디자인 커스텀 완성

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(function () {
-  // 대상을 변수에 저장
+  // 1. 셀렉트박스 이용
   const $selectMenu = $('.select-menu');
 
   // 셀렉트박스를 클릭하면 --> option의 선택이 바뀌면
@@ -14,5 +14,29 @@ $(function () {
 
     // 새 창 열기 --> window.open()
     window.open(linkValue);
+  });
+
+  // 2. 디자인 커스텀
+  const $selectWrap = $('.select-wrap');
+  const $selectList = $('.select-list');
+
+  // strong(Family site)을 $selectList가 보이게
+  $selectWrap.find('strong').on('click', function () {
+    $selectList.slideToggle();
+  });
+
+  $selectList.children('li').on('click', function () {
+    // attr('속성명')
+    // const linkValue = $(this).attr('data-link');
+
+    // data('속성명') = attr('data-속성명')
+    const linkValue = $(this).data('link');
+
+    /* 위에 똑같은 상수명이 있는데 왜 오류가 안날까?
+    const나 let은 중괄호를 지역범위(전국구가 X)로 가지기 때문에 겹치지 않는 걸로 인식 */
+    console.log(linkValue);
+
+    window.open(linkValue);
+    $selectList.slideUp();
   });
 });
