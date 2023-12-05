bb3087dcae435540243a3f8d6e623c0ec07a1dc2
jungeun <yangje1113@naver.com>
2023-12-05 13:04:05 +0900

    서브메뉴 완성

--- 04-hamburger-menu/js/script.js
+++ 04-hamburger-menu/js/script.js
@@ -11,51 +11,11 @@ $(function () {
   // 햄버거 버튼을 클릭했을 때
   $btnMenu.on('click', function (e) {
     e.preventDefault(); /* a의 기본동작을 무력화 : 이거 안하면 계속 a 링크로 넘어가려 할 것임 */
-    // 버튼에 active 클래스 토글
-    // $(this).toggleClass('active');
-
-    // // $dim 활성화
-    // $dim.fadeToggle();
-
-    //   // $menu가 보여지게 : isActive 조건에 따라서
-    //   if (isActive === false) {
-    //     // $menu를 보이게
-    //     $menu.animate(
-    //       {
-    //         left: 0,
-    //       },
-    //       350
-    //     );
-
-    //     isActive = true;
-    //     /* 여기서 true로 다시 돌려줘야 다시 false로 감 */
-    //   } else {
-    //     // $menu를 안보이게
-    //     $menu.animate(
-    //       {
-    //         left: '-100%',
-    //       },
-    //       350
-    //     );
-
-    //     isActive = false;
-    //     /* 여기서는 다시 false로 */
-    //   }
-    // });
 
     isActive === false ? openMenu() : closeMenu();
     // = !isActive ? openMenu() : closeMenu();
   });
 
-  // $dim을 클릭했을 때
-  // $dim.on('click', function () {
-  //   // 1. $dim이 fadeOut
-  //   $dim.fadeOut();
-
-  //   // 2. $menu를 안보이게
-  //   slideMenu('-100%');
-  //   isActive = false;
-  // });
   $dim.on('click', closeMenu);
 
   // 공통의 동작을 함수로 정의
@@ -63,7 +23,7 @@ $(function () {
   function slideMenu(pos) {
     /* pos:매개변수 / 매개변수 이름은 내 마음대로 /
     (pos, dur) 포지션 지속시간 이런식으로 여러개 매개변수로 잡아줄 수 있음 */
-    $menu.animate(
+    $menu.stop().animate(
       {
         left: pos,
       },
@@ -74,15 +34,44 @@ $(function () {
   // 2. openMenu : 메뉴를 보이게(메뉴, active부여, dim fadeIn, isActive true)
   function openMenu() {
     $btnMenu.addClass('active');
-    $dim.fadeIn();
+    $dim.stop().fadeIn();
     slideMenu(0);
     isActive = true;
   }
   // 3. closeMenu : 메뉴를 안보이게(메뉴, active삭제, dim fadeOut, isActive false)
   function closeMenu() {
     $btnMenu.removeClass('active');
-    $dim.fadeOut();
+    $dim.stop().fadeOut();
     slideMenu('-100%');
     isActive = false;
+
+    // 서브메뉴 초기화
+    // $menuList.removeClass('on');
+    // $submenu.slideUp();
+    initSubmenu();
+  }
+
+  // 서브메뉴 동작
+  const $menuList = $('.menu >li');
+  const $submenu = $('.submenu');
+
+  $menuList.on('click', function (e) {
+    e.preventDefault();
+
+    // $menuList.removeClass('on');
+    /* not this로 쓰려면 FAQ 파일 참고 선택된 this를 상수로 정의해주고 그 앞에 not(클릭된 this를 표현하는 상수) 이렇게 써줘야함 */
+
+    // $(this).siblings().removeClass('on');
+    // $(this).siblings().find($submenu).slideUp();
+    initSubmenu();
+
+    $(this).toggleClass('on');
+    $(this).find($submenu).stop().slideToggle();
+  });
+
+  // 서브메뉴 초기화를 함수로 분리
+  function initSubmenu() {
+    $submenu.stop().slideUp();
+    $menuList.removeClass('on');
   }
 });
